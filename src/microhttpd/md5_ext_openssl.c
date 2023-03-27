d6e67d99a65997d8b043455040d78bdcf7992b31
Edouard LEFIZELIER <edouardlefi@gmail.com>
2023-03-27 13:38:46 +0200

    modificaitons de md5_ext_openssl.c

--- src/microhttpd/md5_ext_openssl.c
+++ src/microhttpd/md5_ext_openssl.c
@@ -28,72 +28,19 @@
 #include "md5_ext.h"
 #include "mhd_assert.h"
 
-/**
- * Initialise structure for MD5 calculation.
- *
- * @param ctx the calculation context
- */
 void
-MHD_MD5_init (struct Md5Ctx *ctx)
+MHD_MD5 (struct Md5CtxExt *ctx,
+         const unsigned char *d,
+         size_t n)
 {
-  MD5_CTX c;
-  /*Initial Hash value, see OpenSSL manual*/
-  if (1 = MD5_INIT (&c))
+  unsigned char *md;
+  if (NULL == MD5 (d, n, md))
   {
-    ctx->H[0] = c->A;
-    ctx->H[1] = c->B;
-    ctx->H[2] = c->C;
-    ctx->H[3] = c->D;
-    /* Initialise the number of bytes. */
-    ctx->count = 0;
+    ctx->ext_error = 1;
   }
-  OPENSSL_cleanse (c, sizeof(*c));
+  else
+  {
+    ctx->ext_error = 0;
+    memcpy (ctx->handle, md, sizeof(ctx->H));
+  }
 }
-
-
-/* à terminer */
-/**
- * Process portion of bytes.
- *
- * @param ctx the calculation context
- * @param data bytes to add to hash
- * @param length number of bytes in @a data
- */
-void
-MHD_MD5_update (struct Md5CtxExt *ctx,
-                const uint8_t *data,
-                size_t length)
-{
-  MD5_CTX c;
-  if (0 == ctx->ext_error)
-    /* MD5_Update() return 1 for success, 0 otherwise.*/
-    ctx->ext_error = ! MD5_Update (c, data, length);
-
-}
-
-
-/* à terminer */
-/**
- * Finalise MD5 calculation, return digest, reset hash calculation.
- *
- * @param ctx the calculation context
- * @param[out] digest set to the hash, must be #MD5_DIGEST_SIZE bytes
- */
-void
-MHD_MD5_finish_reset (struct Md5CtxExt *ctx,
-                      uint8_t digest[MD5_DIGEST_SIZE])
-{
-  if (0 == ctx->ext_error)
-    gnutls_hash_output (ctx->handle, digest);
-}
-
-
-/*
-typedef struct MD5state_st
-  101     {
-  102     MD5_LONG A,B,C,D;
-  103     MD5_LONG Nl,Nh;
-  104     MD5_LONG data[MD5_LBLOCK];
-  105     unsigned int num;
-  106     } MD5_CTX;
-*/
